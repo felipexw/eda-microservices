6e861b3acafd71ef972964765c8530ea3594b859
Felipe Appio <felipexw@gmail.com>
2021-03-29 20:59:45 -0300

    Fix sync message bug

--- billing-service/billing-worker.js
+++ billing-service/billing-worker.js
@@ -1,7 +1,7 @@
 const amqp = require('amqplib/callback_api');
 const opt = { credentials: require('amqplib').credentials.plain('root', 'root') };
-const orderPLaceSuccessfully = 'order.place.finished';
-const orderPlaceFail = 'order.place.fail';
+const orderPlaceSuccessfully = 'order-place.finished';
+const orderPlaceFailed = 'order-place.failed.by-billing';
 const orderInventoryFinished = 'order.inventory.finished';
 
 const orderExchangeName = 'order_exchange'
@@ -10,6 +10,12 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
     if (error0) {
         throw error0;
     }
+
+    subscribeToInventoryEvt(connection);
+});
+
+
+function subscribeToInventoryEvt(connection) {
     connection.createChannel(function (error1, channel) {
         if (error1) {
             throw error1;
@@ -25,29 +31,30 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
             if (error2) {
                 throw error2;
             }
-            console.log(' [billing-worker] Waiting for logs. To exit press CTRL+C');
+            console.log(' --- [billing-worker-success] Waiting for logs. To exit press CTRL+C ---');
 
             channel.bindQueue(q.queue, orderExchangeName, orderInventoryFinished);
 
             channel.consume(q.queue, function (msg) {
-                console.log(" [billing-worker] %s %s:'%s'", new Date(), msg.fields.routingKey, msg.content.toString());
+                if (msg.fields.routingKey === orderInventoryFinished) {
+                    console.log(" [billing-worker-success] %s %s:'%s'", new Date(), msg.fields.routingKey, msg.content.toString());
 
-                const order = JSON.parse(msg.content.toString());
-                if (order.reject === 'order.billing') {
-                    //console.log(" [inventory-worker] %s %s:'%s'", new Date(), msg.fields.routingKey, ' order inventory provisioning failed. order id: ' + order.id);
-                    const data = { serviceName: 'billing-service', orderId: order.id }
-                    setTimeout(() => {
-                        channel.publish(orderExchangeName, orderPlaceFail, Buffer.from(JSON.stringify(data)));
-                    }, 1250)
-                }
-                else {
-                    setTimeout(() => {
-                        channel.publish(orderExchangeName, orderPLaceSuccessfully, Buffer.from(msg.content.toString()));
-                    }, 1250)
+                    const order = JSON.parse(msg.content.toString());
+                    if (order.reject === 'order.billing') {
+                        const data = { serviceName: 'billing-service', orderId: order.id }
+                        setTimeout(() => {
+                            channel.publish(orderExchangeName, orderPlaceFailed, Buffer.from(JSON.stringify(data)));
+                        }, 1250)
+                    }
+                    else {
+                        setTimeout(() => {
+                            channel.publish(orderExchangeName, orderPlaceSuccessfully, Buffer.from(msg.content.toString()));
+                        }, 1250)
+                    }
                 }
             }, {
                 noAck: true
             });
         });
     });
-});
+}
--- inventory-service/inventory-worker-success.js
+++ inventory-service/inventory-worker-success.js
@@ -1,8 +1,9 @@
 const amqp = require('amqplib/callback_api');
 const opt = { credentials: require('amqplib').credentials.plain('root', 'root') };
-const orderPlaceStartTopic = 'order.place.start';
-const orderPlaceFail = 'inventory.order.rejected';
+const orderPlaceStartTopic = 'order-place.start';
+const orderPlaceFail = 'order-place.failed.by-inventory';
 const orderInventoryFinished = 'order.inventory.finished';
+const orderBillingRejected = 'order-place.failed.by-billing';
 
 const orderExchangeName = 'order_exchange'
 
@@ -10,6 +11,12 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
     if (error0) {
         throw error0;
     }
+
+    subscribeToOrderBillingEvt(connection);
+    subscribeToOrderPlaceEvt(connection);
+});
+
+function subscribeToOrderPlaceEvt(connection) {
     connection.createChannel(function (error1, channel) {
         if (error1) {
             throw error1;
@@ -25,44 +32,7 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
             if (error2) {
                 throw error2;
             }
-            console.log(' [inventory-worker] Waiting for logs. To exit press CTRL+C');
-
-            channel.bindQueue(q.queue, orderExchangeName, 'billing.order.rejected');
-
-            channel.consume(q.queue, function (msg) {
-                console.log(" [inventory-worker-rejected] %s %s:'%s'", new Date(), msg.fields.routingKey, msg.content.toString());
-
-                const order = JSON.parse(msg.content.toString());
-                if (order.reject === 'order.inventory') {
-                    //console.log(" [inventory-worker] %s %s:'%s'", new Date(), msg.fields.routingKey, ' order inventory provisioning failed. order id: ' + order.id);
-                    const data = { serviceName: 'inventory-service', orderId: order.id }
-                    channel.publish(orderExchangeName, orderPlaceFail, Buffer.from(JSON.stringify(data)));
-                }
-                else {
-                    channel.publish(orderExchangeName, orderInventoryFinished, Buffer.from(msg.content.toString()));
-                }
-            }, {
-                noAck: true
-            });
-        });
-    });
-
-    connection.createChannel(function (error1, channel) {
-        if (error1) {
-            throw error1;
-        }
-
-        channel.assertExchange(orderExchangeName, 'topic', {
-            durable: false
-        });
-
-        channel.assertQueue('', {
-            exclusive: true
-        }, function (error2, q) {
-            if (error2) {
-                throw error2;
-            }
-            console.log(' [inventory-worker] Waiting for logs. To exit press CTRL+C');
+            console.log(' --- [inventory-worker] Waiting for logs. To exit press CTRL+C --- ');
 
             channel.bindQueue(q.queue, orderExchangeName, orderPlaceStartTopic);
 
@@ -71,9 +41,9 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
 
                 const order = JSON.parse(msg.content.toString());
                 if (order.reject === 'order.inventory') {
-                    //console.log(" [inventory-worker] %s %s:'%s'", new Date(), msg.fields.routingKey, ' order inventory provisioning failed. order id: ' + order.id);
                     const data = { serviceName: 'inventory-service', orderId: order.id }
                     setTimeout(() => {
+                        console.log(` [inventory-worker] order rejected by inventory worker. Order data: ${msg.content.toString()} `);
                         channel.publish(orderExchangeName, orderPlaceFail, Buffer.from(JSON.stringify(data)));
                     }, 1250)
                 }
@@ -87,4 +57,42 @@ amqp.connect('amqp://localhost', opt, function (error0, connection) {
             });
         });
     });
-});
+}
+
+function subscribeToOrderBillingEvt(connection) {
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        channel.assertExchange(orderExchangeName, 'topic', {
+            durable: false
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function (error2, q) {
+            if (error2) {
+                throw error2;
+            }
+            console.log(' --- [inventory-worker-succes] Waiting for logs. To exit press CTRL+C --- ');
+
+            channel.bindQueue(q.queue, orderExchangeName, orderBillingRejected);
+
+            channel.consume(q.queue, function (msg) {
+                console.log(" [inventory-worker-rejected] %s %s:'%s'", new Date(), msg.fields.routingKey, msg.content.toString());
+
+                const order = JSON.parse(msg.content.toString());
+                if (order.reject === 'order.inventory') {
+                    const data = { serviceName: 'inventory-service' }
+                    channel.publish(orderExchangeName, orderPlaceFail, Buffer.from(JSON.stringify(data)));
+                }
+                else {
+                    channel.publish(orderExchangeName, orderInventoryFinished, Buffer.from(msg.content.toString()));
+                }
+            }, {
+                noAck: true
+            });
+        });
+    });
+}
